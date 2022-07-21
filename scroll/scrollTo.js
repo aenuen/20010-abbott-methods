9b99308d1f3b4c46694596d68a4c58aa8426c5b6
aenuen <aenuen@qq.com>
2022-07-21 18:05:12 +0800

    upgrade

---
+++ scroll/scrollTo.js
@@ -0,0 +1,58 @@
+// noinspection JSUnresolvedVariable
+
+/**
+ * @param {number} to
+ * @param {number} duration
+ * @param {Function} [callback]
+ */
+const scrollTo = (to, duration, callback) => {
+
+  const position = () => document.documentElement.scrollTop || document.body.parentNode.scrollTop || document.body.scrollTop
+
+
+  const start = position()
+  const change = to - start
+  const increment = 20
+  let currentTime = 0
+  duration = (typeof (duration) === 'undefined') ? 500 : duration
+
+
+  Math.easeInOutQuad = (t, b, c, d) => {
+    t /= d / 2
+    if (t < 1) {
+      return c / 2 * t * t + b
+    }
+    t--
+    return -c / 2 * (t * (t - 2) - 1) + b
+  }
+
+  const move = amount => {
+    document.documentElement.scrollTop = amount
+    document.body.parentNode.scrollTop = amount
+    document.body.scrollTop = amount
+  }
+
+  const requestAnimFrame = (() => window.requestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
+      window.mozRequestAnimationFrame ||
+      function (callback) {
+        window.setTimeout(callback, 1000 / 60)
+      }
+  )()
+
+  const animateScroll = function () {
+    currentTime += increment
+    const val = Math.easeInOutQuad(currentTime, start, change, duration)
+    move(val)
+    if (currentTime < duration) {
+      requestAnimFrame(animateScroll)
+    } else {
+      if (callback && typeof (callback) === 'function') {
+        callback()
+      }
+    }
+  }
+  animateScroll()
+}
+
+module.exports = scrollTo
