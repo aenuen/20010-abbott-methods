1b373bb67f5832bf45da2c842ce6b44fe37e995d
aenuen <aenuen@qq.com>
2022-12-06 16:57:05 +0800

    upgrade

--- src/some/someLetter26.ts
+++ src/some/someLetter26.ts
@@ -2,11 +2,11 @@
  * @Author: abbott
  * @Date: 2022-12-06 16:34:32
  * @LastEditors: abbott
- * @LastEditTime: 2022-12-06 16:48:23
+ * @LastEditTime: 2022-12-06 16:52:45
  * @Description:
  */
 /**
- * @description 26个小写字母
+ * @description 26个字母 true 大写 无参数或false 小写
  * @param {Boolean} [isBig]
  * @returns {String}
  */
